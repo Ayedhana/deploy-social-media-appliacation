9ceb256cf4d9630cb4207cb1c4fdbf9e45de8113
Ayedhana <119585680+Ayedhana@users.noreply.github.com>
2025-05-22 21:48:29 +0200

    Update2 index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.use('/', postRoutes);
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT|| 5000;
