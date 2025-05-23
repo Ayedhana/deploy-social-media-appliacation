154d0eea99711dc9d9a2dbe2330061f634f1d7a0
Ayedhana <119585680+Ayedhana@users.noreply.github.com>
2025-05-23 16:09:20 +0200

    Update app index.js

--- server/index.js
+++ server/index.js
@@ -7,26 +7,21 @@ import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
-const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+const app = express();
+
 const corsOptions = {
-  origin: 'https://deploy-social-media-appliacation-frontend-7di52kpah.vercel.app', // ton frontend déployé
+  origin: 'https://social-media-appliacation-backend.vercel.app/posts', // ton frontend déployé
   methods: 'GET,POST,PATCH,DELETE',
   credentials: true,
 };
+
 app.use(cors(corsOptions));
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
-const PORT = process.env.PORT|| 5000;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
-
-export default app;
-
+export default app; // nécessaire pour Vercel
