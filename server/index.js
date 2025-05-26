285af9267973bc11f7d103fe17dbbf1a61ed08cf
Ayedhana <119585680+Ayedhana@users.noreply.github.com>
2025-05-26 13:38:55 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: 'https://social-media-appliacation-frontend.vercel.app/', // ton frontend déployé
+  origin: 'https://social-media-appliacation-frontend.vercel.app', // ton frontend déployé
   methods: 'GET,POST,PATCH,DELETE',
   credentials: true,
 };
