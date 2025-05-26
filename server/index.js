4141985f373070b7c337dcb95b9f77649a90e196
Ayedhana <119585680+Ayedhana@users.noreply.github.com>
2025-05-26 14:47:42 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: 'https://social-media-appliacation-frontend.vercel.app', // ton frontend déployé
+  origin: 'https://social-media-appliacation-frontend.vercel.app', 
   methods: 'GET,POST,PATCH,DELETE',
   credentials: true,
 };
@@ -22,6 +22,6 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
-app.use('/posts', postRoutes);
+app.use('/', postRoutes);
 
 export default app; // nécessaire pour Vercel
